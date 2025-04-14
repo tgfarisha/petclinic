4d170eed69adfc5c8ad59da5eef212ffd6ad9bbf
farisha <farisha.hamzah@imocha.com.my>
2025-04-14 17:56:24 +0800

    add on footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { OwnerComponent } from './owner/owner.component';
-import { OwnerAddComponent } from './owner-add/owner-add.component';
-import { OwnerDetailComponent } from './owner-detail/owner-detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
-    HomeComponent,
-    OwnerComponent,
-    OwnerAddComponent,
-    OwnerDetailComponent,
-    RouterOutlet
+    RouterOutlet,
+    FooterComponent
   ],
   template: `
     <app-navbar></app-navbar>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `,
   styleUrls: ['./app.component.css']
 })
